f4add8d51afeb9b53857a504c1ee27829cd8bc11
okdah <okdah@live.com>
2016-04-04 15:39:48 +0200

    a

--- public/javascripts/Controllers/SController.js
+++ public/javascripts/Controllers/SController.js
@@ -1,36 +1,34 @@
 var app = angular.module("myApp")
-  .controller('SController', ['$scope', function($scope) { 
-  // $scope.Flight = [
-  $scope.search = [
-                
-                
-                From: 'Kuala Lampur'
-                To: 'Barcelona',
-                Departure Date: '1/1/2017',
-                Return Date:  '10,1,2017',
-                
+    .controller('SController', ['$scope', function($scope) {
+        // $scope.Flight = [
+        $scope.search = [{
+
+            From: 'Kuala Lampur',
+            To: 'Barcelona',
+            Departure_Date: '1/1/2017',
+            Return_Date: '10,1,2017'
 
 
-            }, {
-                Name: 'Malasia Airlines',
-                price: 490,
-                Destination: 'Paris',
-                Cover: 'images/paris.jpg',
-                Date: '1 / 1 / 2017'
+        }, {
+            Name: 'Malasia Airlines',
+            price: 490,
+            Destination: 'Paris',
+            Cover: 'images/paris.jpg',
+            Date: '1 / 1 / 2017'
 
-            }, {
-                Name: 'Malasia Airlines',
-                price: 390,
-                Destination: 'New York',
-                Cover: 'images/new-york-city.jpg',
-                Date: '1 / 1 / 2017'
+        }, {
+            Name: 'Malasia Airlines',
+            price: 390,
+            Destination: 'New York',
+            Cover: 'images/new-york-city.jpg',
+            Date: '1 / 1 / 2017'
 
-            }, {
-                Name: 'Malasia Airlines',
-                price: 290,
-                Destination: 'London',
-                Cover: 'images/london.jpg',
-                Date: '1 / 1 / 2017'
+        }, {
+            Name: 'Malasia Airlines',
+            price: 290,
+            Destination: 'London',
+            Cover: 'images/london.jpg',
+            Date: '1 / 1 / 2017'
 
-            }
- ]
+        }];
+    }]);
--- public/javascripts/basicApp.js
+++ public/javascripts/basicApp.js
@@ -1,11 +1,16 @@
 angular.module('myApp', [
         'ngRoute'
     ])
-.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/flights', {
-        templateUrl: 'outgoing.html',
-        controller: 'MainController'
-      });
-  }]);
+    .config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+            when('/flights', {
+                    templateUrl: 'outgoing.html',
+                    controller: 'MainController'
+                })
+                .when('/search', {
+                    templateUrl: 'search.html',
+                    controller: 'SController'
+                });
+        }
+    ]);
